89068bff4ff0abb3321f65a0893da9056227e5f7
Daniel <danieldawit33@gmail.com>
2021-11-25 17:51:09 -0500

    port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.listen(port, () => {
-    console.log("Starting server on : http://localhost:"+post)
+    console.log("Starting server on : http://localhost:"+port)
 });
 
 app.get('/api', ((req, res) => {
