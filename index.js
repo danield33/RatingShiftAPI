9ba3b465f5a38b9e7a7a06ae00aa2f2401103dd1
Daniel <danieldawit33@gmail.com>
2021-11-25 19:45:01 -0500

    updates

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors')({origin: true});
+const cheerio = require('cheerio');
+const fetch = require('node-fetch')
 
 app.use(express.static('public'));
 
@@ -8,6 +11,17 @@ app.listen(port, () => {
     console.log("Starting server on : http://localhost:"+port)
 });
 
-app.get('/api', ((req, res) => {
-    res.send("Hello World")
+app.get('/api', (async (req, res) => {
+
+        console.log(fetch, req.query)
+        const response = await fetch('https://fnd.io/#/us/search?mediaType=all&term='+req.query.text)
+        const html = await response.text();
+        const $ = cheerio.load(html);
+
+        console.log(html)
+
+        // const $ = cheerio.load();
+
+        res.send("Hello World")
+
 }))
